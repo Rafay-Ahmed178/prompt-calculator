ad4f5a383e7b93c44c9c8fe1aec64fe4e4a5aebd
inncontrafay <rafayahmed625@gmail.com>
2024-03-14 10:03:39 +0500

    prompt calculator

---
+++ app.js
@@ -0,0 +1,27 @@
+let userName = prompt("What is Your Name");
+alert("welcome " + userName);
+console.log(userName);
+
+let num1 = prompt("Enter any First Num");
+let num2 = prompt("Enter any second num");
+
+num1 = Number(num1);
+num2 = Number(num2);
+console.log("num1: ", num1);
+console.log("num2: ", num2);
+
+let sum = num1 + num2;
+alert("Addition " + sum);
+console.log("Addition " + sum);
+
+let minus = num1 - num2;
+alert("substraction " + minus);
+console.log("substraction " + minus);
+
+let multiply = num1 + num2;
+alert("Multiplication " + num1 * num2);
+console.log("Multiplication " + num1 * num2);
+
+let divide = num1 / num2;
+alert("Division " + num1 / num2);
+console.log("Division " + num1 / num2);
